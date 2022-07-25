d093f5d127f9e8e0847c62f07f5298a4a7e80114
clifotieno97 <72338376+clifotieno97@users.noreply.github.com>
2022-07-25 16:12:34 +0300

    Update initLesson.R

--- Open_Intro/Overview_of_Statistics/initLesson.R
+++ Open_Intro/Overview_of_Statistics/initLesson.R
@@ -1,2 +1,2 @@
-  assign("cars", openintro::cars, envir=globalenv())
-  assign("mpg.midsize", cars[cars$type=="midsize","mpgCity"], envir=globalenv())
+  assign("cars", openintro::cars98, envir=globalenv())
+  assign("mpg.midsize", cars[cars98$type=="midsize","mpg_city"], envir=globalenv())
